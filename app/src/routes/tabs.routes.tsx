32ca2025a4180424597eb392cd7a8f261af9b1e3
impaulinha <anaaguiar20016@gmail.com>
2025-09-18 16:22:31 -0300

    style: tabBar estilizada

--- app/src/routes/tabs.routes.tsx
+++ app/src/routes/tabs.routes.tsx
@@ -1,6 +1,8 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { Characters } from '../screens/Characters'
 import { Favorites } from '../screens/Favorites'
+import Ionicons from '@react-native-vector-icons/ionicons'
+import { theme } from '../global/theme'
 
 export type TabsParamList = {
   Characters: undefined
@@ -11,16 +13,53 @@ const Tab = createBottomTabNavigator()
 
 export function TabsRoutes() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={{
+        tabBarActiveTintColor: theme.colors.primary,
+        tabBarInactiveTintColor: theme.colors.onSurface,
+        tabBarStyle: {
+          position: 'absolute',
+          backgroundColor: theme.colors.secondary,
+          height: 60,
+          marginHorizontal: 20,
+          marginBottom: 45,
+          borderRadius: 50,
+          elevation: 5,
+          paddingTop: 0,
+          paddingBottom: 0,
+        },
+        tabBarItemStyle: {
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+          padding: 0,
+        },
+        tabBarLabelStyle: {
+          margin: 0,
+          padding: 0,
+          fontSize: 12,
+        },
+      }}
+    >
       <Tab.Screen
         name="Characters"
         component={Characters}
-        options={{ headerShown: false }}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person" color={color} size={size} />
+          ),
+        }}
       />
       <Tab.Screen
         name="Favorites"
         component={Favorites}
-        options={{ headerShown: false }}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="heart-sharp" color={color} size={size} />
+          ),
+        }}
       />
     </Tab.Navigator>
   )
